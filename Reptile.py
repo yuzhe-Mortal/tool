6e258b4103d22437f4f7ac345d2f177c71552fe8
yuzhe <52953085+yuzhe-Mortal@users.noreply.github.com>
2021-11-06 22:13:19 +0800

    Update Reptile.py

--- Reptile.py
+++ Reptile.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env python"
-# coding: utf-8
+#!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+# coding=utf-8
 # By yz
+import time
 
-
-import requests, argparse, sys, re
+import requests, argparse, sys, re,hashlib,queue,threading
 from requests.packages import urllib3
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-temp = [".css", ".ico", ".jpg",".png"]
+temp = [".css", ".ico", ".jpg",".png",".vue"]
 allurl=[]
 allhost=[]
 allurl_new=[]
+html_md5 = []
+js_url = []
+url_list=queue.Queue()#待爬取列队
+pass_url=""#404页面
+content_url = ""
 def parse_args():
     parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -u http://www.baidu.com")
     parser.add_argument("-u", "--url", help="The website")
@@ -21,12 +26,13 @@ def parse_args():
     parser.add_argument("-ou", "--outputurl", help="Output file name. ")
     parser.add_argument("-os", "--outputsubdomain", help="Output file name. ")
     parser.add_argument("-d", "--deep", help="Deep find", action="store_true")
+    parser.add_argument("-t", "--thread", help="Multithreading")
     return parser.parse_args()
 
 # Regular expression comes from https://github.com/GerbenJavado/LinkFinder
 def extract_REURL(html):
     pattern_raw = r"""
-      (?:"|')                              # Start newline delimiter
+      ((?:"|')                              # Start newline delimiter
       (
         ((?:[a-zA-Z]{1,10}://|//)           # Match a scheme [a-Z]*1-10 or //
         [^"'/]{1,}\.                        # Match a domainname (any character + dot)
@@ -50,32 +56,50 @@ def extract_REURL(html):
              action|html|js|txt|xml|do)             # . + extension
         (?:[\?|#][^"|']{0,}|))                  # ? mark with parameters
       )
-      (?:"|')                               # End newline delimiter
+      (?:"|')
+      |
+      (href=[^>\s]*?\.js)
+      )                               # End newline delimiter
     """
     pattern = re.compile(pattern_raw, re.VERBOSE)
     result = re.finditer(pattern, str(html))
     if result == None:
         return None
-    js_url = []
+
     js_file = ["text/javascript", "text/css", "multipart/form-data", "application/x-www-form-urlencoded",
                "application/json", "text/javascript", "text/javascript", "text/plain", "text/xml", "application/xml",
                "application/pdf","text/html "]
     for match in result:
-        match = match.group().strip('"').strip("'")
+        match = match.group().strip('"').strip("'").lstrip('href=')
         if match not in js_url and match not in js_file:
             js_url.append(match)
     return js_url
 
 def Extract_html(URL,status_code=False):
     requests.packages.urllib3.disable_warnings()
+    md5 = hashlib.md5()
+    res_header=[]
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
         "Cookie": args.cookie}
     try:
+        if "vmobile.douyu.com" in URL:
+            return None
         raw_old = requests.get(URL, headers=header, timeout=3, verify=False)
         raw = raw_old.content.decode("utf-8", "ignore")
+        md5.update(raw.encode('utf-8'))
+        md5_value=md5.hexdigest()
+        size=len(raw)
+        if raw_old.status_code ==404:
+            return None
+        if md5_value not in html_md5:
+            html_md5.append(md5_value)
+        elif status_code==False:
+            return None
         if status_code==True:
-            return raw_old.status_code
+            res_header.append(str(raw_old.status_code))
+            res_header.append(str(size))
+            return res_header
         else:
             return raw
     except:
@@ -97,7 +121,10 @@ def process_url(URL, re_URL):  # 将相对路径转换为URL
     host_URL = URL_raw.scheme  # 获取协议
     path_URL = URL_raw.path
     re_URL= re_URL.split('?')[0]
-    result = urljoin(host_URL + "://" + ab_URL + path_URL, re_URL)
+    try:
+        result = urljoin(host_URL + "://" + ab_URL + path_URL, re_URL)
+    except:
+        return None
     if result!=URL:return result
     else:return None
 
@@ -144,13 +171,19 @@ def find_by_url_deep():
                 find_by_url(i)
 
 def giveresult():
-    content_url = ""
+
     content_domain = ""
+    resultThread = []
     print("Find "+str(len(allurl))+" URL")
     for url in allurl:
-        print(url+"   "+str(Extract_html(url,True)))
-        content_url += url + "\n"
-    print("\nFind " + str(len(allhost)) + " Subdomain:")
+        url_list.put(url)
+    for i in range(10):  # 创建线程表
+        resultThread.append(threading.Thread(target=url_state_thread))
+    for i in resultThread:  # 开启线程表
+        i.start()
+    for i in resultThread:
+        i.join()
+    print(pass_url)
     for domain in allhost:
         print("Subdomain:" + domain)
         content_domain += domain + "\n"
@@ -174,6 +207,18 @@ def find_by_file(file_path):
     for link in links:
         find_by_url(link)
         find_by_url_deep()
+
+def url_state_thread():
+    global pass_url,content_url
+    while not url_list.empty():
+        url=url_list.get()
+        res_header = Extract_html(url, True)
+        if res_header == None:
+            pass_url +=url+"\n"
+            continue
+        print(url + "  状态码：" + res_header[0] + "  szie: " + res_header[1])
+        content_url += url + "\n"
+
 
 
 if __name__ == "__main__":
